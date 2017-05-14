d090470e19db79eb30b7793f950f97b0c622d0e8
Senthuran Sivananthan <senthuran.sivananthan@microsoft.com>
2017-05-13 23:46:44 -0400

    Externalized environment variables

--- service-map/c#/OMSServiceMapExport/Program.cs
+++ service-map/c#/OMSServiceMapExport/Program.cs
@@ -3,6 +3,8 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
@@ -11,22 +13,24 @@ namespace OMSServiceMapExport
 {
     class Program
     {
-        static string SUBSCRIPTION_ID = "SUBSCRIPTION-GUID";
+        static string SUBSCRIPTION_ID = ConfigurationManager.AppSettings["SubscriptionId"];
 
-        static string TENANT_ID = "AZURE-AD-GUID";
-        static string CLIENT_ID = "APPLICATION-ID-GUID";
-        static string CLIENT_SECRET = "APPLICATION-SECRET";
+        static string TENANT_ID = ConfigurationManager.AppSettings["AADTenantId"];
+        static string CLIENT_ID = ConfigurationManager.AppSettings["AADClientId"];
+        static string CLIENT_SECRET = ConfigurationManager.AppSettings["AADClientSecret;
 
         static string MANAGEMENT_URL_REST = "https://management.azure.com";
         static string SERVICE_MAP_API_VERSION = "2015-11-01-preview";
 
-        static string OMS_WORKSPACE_RESOURCE_GROUP = "";
-        static string OMS_WORKSPACE_NAME = "";
+        static string OMS_WORKSPACE_RESOURCE_GROUP = ConfigurationManager.AppSettings["OMSWorkspaceResourceGroup"];
+        static string OMS_WORKSPACE_NAME = ConfigurationManager.AppSettings["OMSWorkspaceName"];
 
         static void Main(string[] args)
         {
             string accessToken = Login();
             JObject machines = GetMachines(accessToken);
+
+            GetProcesses(accessToken, machines);
             List<JObject> serviceMaps = GetServiceMaps(accessToken, machines);
 
             Console.ReadLine();
@@ -43,8 +47,7 @@ namespace OMSServiceMapExport
 
         static HttpClient GetServiceMapClient(string accessToken)
         {
-            string url = string.Format("{0}/subscriptions/{1}/resourceGroups/{2}/providers/Microsoft.OperationalInsights/workspaces/{3}/features/serviceMap/",
-                                MANAGEMENT_URL_REST, SUBSCRIPTION_ID, OMS_WORKSPACE_RESOURCE_GROUP, OMS_WORKSPACE_NAME);
+            string url = $"{MANAGEMENT_URL_REST}/subscriptions/{SUBSCRIPTION_ID}/resourceGroups/{OMS_WORKSPACE_RESOURCE_GROUP}/providers/Microsoft.OperationalInsights/workspaces/{OMS_WORKSPACE_NAME}/features/serviceMap/";
 
             var client = new HttpClient()
             {
@@ -52,7 +55,7 @@ namespace OMSServiceMapExport
             };
 
             client.DefaultRequestHeaders.Accept.Clear();
-            client.DefaultRequestHeaders.Add("Authorization", string.Format("Bearer {0}", accessToken));
+            client.DefaultRequestHeaders.Add("Authorization", $"Bearer {accessToken}");
             return client;
         }
 
@@ -61,20 +64,43 @@ namespace OMSServiceMapExport
             var client = GetServiceMapClient(accessToken);
             client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
-            var result = client.GetAsync(string.Format("machines?api-version={0}", SERVICE_MAP_API_VERSION));
+            var result = client.GetAsync($"machines?api-version={SERVICE_MAP_API_VERSION}");
             var content = result.Result.Content.ReadAsStringAsync();
             var machines = JsonConvert.DeserializeObject(content.Result) as JObject;
             
             return machines;
         }
 
+        static void GetProcesses(string accessToken, JObject machines)
+        {
+            var client = GetServiceMapClient(accessToken);
+
+            foreach (var machine in machines)
+            {
+                if (machine.Value.First == null)
+                {
+                    continue;
+                }
+
+                var machineId = machine.Value.First.SelectToken("id").Value<string>().Split('/').Last();
+                var result = client.GetAsync($"machines/{machineId}/processes?api-version={SERVICE_MAP_API_VERSION}");
+                var content = result.Result.Content.ReadAsStringAsync();
+                Console.WriteLine(content);
+            }
+        }
+
         static List<JObject> GetServiceMaps(string accessToken, JObject machines)
         {
             var client = GetServiceMapClient(accessToken);
             var serviceMaps = new List<JObject>();
-            
+
             foreach (var machine in machines)
             {
+                if (machine.Value.First == null)
+                {
+                    continue;
+                }
+
                 var machineId = machine.Value.First.SelectToken("id").Value<string>();
 
                 var postContent = new StringContent(
@@ -86,7 +112,7 @@ namespace OMSServiceMapExport
                         Encoding.UTF8,
                         "application/json");
 
-                var result = client.PostAsync(string.Format("generateMap?api-version={0}", SERVICE_MAP_API_VERSION), postContent);
+                var result = client.PostAsync($"generateMap?api-version={SERVICE_MAP_API_VERSION}", postContent);
                 var content = result.Result.Content.ReadAsStringAsync();
 
                 serviceMaps.Add(JsonConvert.DeserializeObject(content.Result) as JObject);
